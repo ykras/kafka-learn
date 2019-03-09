e34ad17841ec850420296ff71581a5f23d371249
ykrasnov <ykrasnov@anresh.ru>
2019-03-09 16:40:01 +0300

    consume json val

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,7 +1,9 @@
 from kafka import KafkaConsumer
+import json
 
 consumer = KafkaConsumer('spider_eis_fz44', bootstrap_servers='stable-kafka-1:9092', auto_offset_reset='latest')
 
 for msg in consumer:
-    print(msg)
+    val = json.load(msg.val)
+    print(val)
 
